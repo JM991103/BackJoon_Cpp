76a302c35f9fbc32fb3b436c2c308a4efda51bc6
JM991103 <ljn1103@naver.com>
2023-02-22 15:36:12 +0900

    문제 1번 해결

--- Day_1/Day_1/day_1.cpp
+++ Day_1/Day_1/day_1.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+	int n = 0;
+	string numbers;
+	cin >> n;
+	cin >> numbers;
 
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += numbers[i] - '0';
+	}
+	cout << sum << "\n";
 }
