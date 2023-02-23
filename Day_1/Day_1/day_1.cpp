514342e98efd9696d70e97c1293579c8acf49378
JM991103 <ljn1103@naver.com>
2023-02-23 16:03:14 +0900

    2일차 백준 문제 풀기

--- Day_1/Day_1/day_1.cpp
+++ Day_1/Day_1/day_1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	int n = 0;
+	/*int n = 0;
 	string numbers;
 	cin >> n;
 	cin >> numbers;
@@ -13,5 +13,30 @@ int main()
 	{
 		sum += numbers[i] - '0';
 	}
-	cout << sum << "\n";
+	cout << sum << "\n";*/
+
+	int n = 0;
+	int a[1000];
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	long sum = 0;
+	long max = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+		sum = sum + a[i];
+	}
+
+	// 한 과목 관련된 수식을 모두 더한 후 관련된 수식으로 변환해 로직이 간단해짐
+	double result = sum * 100.0 / max / n;
+	cout << result << "\n";
 }
---
+++ Day_1/Day_2/day_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//int n = 0;
+	//int a[1000];
+	//cin >> n;
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cin >> a[i];
+	//}
+
+	//long sum = 0;
+	//long max = 0;
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	if (a[i] > max)
+	//	{
+	//		max = a[i];
+	//	}
+	//	sum = sum + a[i];
+	//}
+
+	//// 한 과목 관련된 수식을 모두 더한 후 관련된 수식으로 변환해 로직이 간단해짐
+	//double result = sum * 100.0 / max / n;
+	//cout << result << "\n";
+
+
+	//long long a, b, c;
+
+	//cin >> a >> b >> c;
+	//cout << a + b + c;
+
+
+	//int dan = 0;
+
+	//cin >> dan;
+
+	//for (int i = 1; i < 10; i++)
+	//{
+	//	cout << dan << " * " << i << " = " << dan * i << endl;
+	//}
+
+
+
+
+	return 0;
+}
