a26a81e1dbfc61fab9ca7ac240fc2ead6b9d5cde
JM991103 <ljn1103@naver.com>
2023-02-27 19:21:46 +0900

    6일차 백준 풀기

--- Day_1/Day_3/day_3.cpp
+++ Day_1/Day_3/day_3.cpp
@@ -44,16 +44,88 @@ int main()
 	//}
 	//cout << m << "\n";
 
-	int n = 0;
-	cin >> n;
+	//int n = 0;
+	//cin >> n;
 
-	for (int i = 1; i <= n; i++)
+	//for (int i = 1; i <= n; i++)
+	//{
+	//	int x;
+	//	int y;
+	//	cin >> x >> y;
+
+	//	cout << "Case #" << i << ": " << x << " + " << y << " = " << x + y << "\n";
+	//}
+
+	//int n = 0;
+	//cin >> n;
+	//int m[101] = { 0 };
+	//int count = 0;
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	int s;
+	//	cin >> s;
+	//	m[i] = s;
+	//}
+
+	//int v;
+	//cin >> v;
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	if (v == m[i])
+	//	{
+	//		count++;
+	//	}
+	//}
+
+	//cout << count;
+
+	//int n, m;
+	//int x, y, z;
+	//int count = 0;
+	//int arr[101] = { 0 };
+	//cin >> n >> m;
+
+	//for (int i = 0; i < m; i++)
+	//{
+	//	cin >> x >> y >> z;
+
+	//	for (int j = x; j <= y; j++)
+	//	{
+	//		arr[j] = z;
+	//	}		
+	//}
+
+	//for (int i = 1; i <= n; i++)
+	//{
+	//	count++;
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+
+	int n, m;
+	int x, y;
+	int temp;
+	int arr[101] = { 0 };
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{				
+		arr[i] = i + 1;		
+	}
+
+	for (int i = 0; i < m; i++)
 	{
-		int x;
-		int y;
 		cin >> x >> y;
+		temp = arr[x - 1];
+		arr[x - 1] = arr[y - 1];
+		arr[y - 1] = temp;
+	}
 
-		cout << "Case #" << i << ": " << x << " + " << y << " = " << x + y << "\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
 	}
 
 	return 0;
