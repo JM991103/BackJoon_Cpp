bc84b99287e5739d41d24dd3751d1fa70278714a
JM991103 <ljn1103@naver.com>
2023-03-06 16:50:21 +0900

    13일차 백준 풀기

---
+++ Day_1/Day_13/day_13_2cpp.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+/// <summary>
+/// 비밀번호 체크 배열
+/// </summary>
+int checkArr[4];
+
+/// <summary>
+/// 현재 상태 배열
+/// </summary>
+int myArr[4];
+
+/// <summary>
+/// 몇 개의 문자에 대한 개수를 충족했는지 판단하는 함수
+/// </summary>
+int checkSecet = 0;
+
+void Add(char c);
+void Remove(char c);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	cout.tie(NULL);
+
+	/// <summary>
+	/// 문자열 크기
+	/// </summary>
+	int s;
+
+	/// <summary>
+	/// 부분 문자열 크기
+	/// </summary>
+	int	p;
+	std::cin >> s >> p;
+	int result = 0;
+
+	/// <summary>
+	/// 문자열 데이터
+	/// </summary>
+	string A;
+	std::cin >> A;
+
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> checkArr[i];
+		if (checkArr[i] == 0)
+		{
+			checkSecet++;
+		}
+	}
+
+	for (int i = 0; i < p; i++)
+	{
+		Add(A[i]);
+	}
+
+	if (checkSecet == 4)
+	{
+		result++;
+	}
+
+	for (int i = p; i < s; i++)
+	{
+		int j = i - p;
+		Add(A[i]);
+		Remove(A[j]);
+		if (checkSecet == 4)
+		{
+			result++;
+		}
+	}
+
+	cout << result << endl;
+}
+
+void Add(char c)
+{
+	switch (c)
+	{
+	case 'A':
+		myArr[0]++;
+		if (myArr[0] == checkArr[0])
+		{
+			checkSecet++;
+		}
+		break;
+	case 'C':
+		myArr[1]++;
+		if (myArr[1] == checkArr[1])
+		{
+			checkSecet++;
+		}
+		break;
+	case 'G':
+		myArr[2]++;
+		if (myArr[2] == checkArr[2])
+		{
+			checkSecet++;
+		}
+		break;
+	case 'T':
+		myArr[3]++;
+		if (myArr[3] == checkArr[3])
+		{
+			checkSecet++;
+		}
+		break;
+	}
+}
+
+void Remove(char c)
+{
+	switch (c)
+	{
+	case 'A':		
+		if (myArr[0] == checkArr[0])
+		{
+			checkSecet--;
+		}
+		myArr[0]--;
+		break;
+	case 'C':
+		if (myArr[1] == checkArr[1])
+		{
+			checkSecet--;
+		}
+		myArr[1]--;
+		break;
+	case 'G':
+		if (myArr[2] == checkArr[2])
+		{
+			checkSecet--;
+		}
+		myArr[2]--;
+		break;
+	case 'T':
+		if (myArr[3] == checkArr[3])
+		{
+			checkSecet--;
+		}
+		myArr[3]--;
+		break;
+	}
+}
