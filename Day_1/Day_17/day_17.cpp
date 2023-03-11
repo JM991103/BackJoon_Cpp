6de3577e0ee8e5aaad9c39056eb5a00eff6d5650
JM991103 <ljn1103@naver.com>
2023-03-11 16:52:51 +0900

    18일차 백준 풀기

--- Day_1/Day_17/day_17.cpp
+++ Day_1/Day_17/day_17.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
@@ -8,33 +9,46 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	queue<int> myqueue;
-	int n;
-	cin >> n;
+	//queue<int> myqueue;
+	//int n;
+	//cin >> n;
 
-	//for (int i = 1; i <= n; i++)	// 카드를 큐에 저장하기
+	////for (int i = 1; i <= n; i++)	// 카드를 큐에 저장하기
+	////{
+	////	myqueue.push(i);
+	////}
+
+	////while (myqueue.size() > 1)	// 카드가 1장 남을 때까지
+	////{
+	////	myqueue.pop();	// 맨 위 카드를 버림
+	////	myqueue.push(myqueue.front());	// 맨 위 카드를 가장 아래 카드 밑으로 이동
+	////	myqueue.pop();
+	////}
+
+	//for (int i = 1; i <= n; i++)
 	//{
 	//	myqueue.push(i);
 	//}
 
-	//while (myqueue.size() > 1)	// 카드가 1장 남을 때까지
+	//while (myqueue.size() > 1)
 	//{
-	//	myqueue.pop();	// 맨 위 카드를 버림
-	//	myqueue.push(myqueue.front());	// 맨 위 카드를 가장 아래 카드 밑으로 이동
+	//	myqueue.pop();
+	//	myqueue.push(myqueue.front());
 	//	myqueue.pop();
 	//}
 
-	for (int i = 1; i <= n; i++)
+	//cout << myqueue.front();
+
+	int n = 0;
+	vector<int> a(5, 0);
+
+	for (int i = 0; i < a.size(); i++)
 	{
-		myqueue.push(i);
+		cin >> a[i];
+		n += a[i] * a[i];
 	}
 
-	while (myqueue.size() > 1)
-	{
-		myqueue.pop();
-		myqueue.push(myqueue.front());
-		myqueue.pop();
-	}
+	int b = n % 10;
 
-	cout << myqueue.front();
+	cout << b;
 }
