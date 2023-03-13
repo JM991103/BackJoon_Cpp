71ee111854480cfbf245c408a8cbc60f86340ffc
JM991103 <ljn1103@naver.com>
2023-03-13 13:39:25 +0900

    20일차 백준 풀기

---
+++ Day_1/Day_20/day_20cpp.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct compare {
+	bool operator()(int o1, int o2) {
+		int first_abs = abs(o1);
+		int second_abs = abs(o2);
+		if (first_abs == second_abs)
+		{
+			return o1 > o2;		// 절댓값이 같을 때는 음수 우선 정렬
+		}
+		else
+		{
+			return first_abs > second_abs;	// 절댓값을 기준으로 정렬
+		}
+	}
+};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 우선순위 큐 선언 방식 : <자료형, 구현체, 비교 함수명>
+	priority_queue<int, vector<int>, compare> MyQueue;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int request;
+		cin >> request;
+
+		if (request == 0)
+		{
+			if (MyQueue.empty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << MyQueue.top() << "\n";
+				MyQueue.pop();
+			}
+		}
+		else
+		{
+			MyQueue.push(request);
+		}
+	}
+}
