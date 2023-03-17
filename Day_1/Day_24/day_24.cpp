c1116ed12d96f795780df9e78b1d07e8dcae2b25
JM991103 <ljn1103@naver.com>
2023-03-17 18:06:07 +0900

    24일차 백준 풀기

---
+++ Day_1/Day_24/day_24.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> a(n, 0);
+	vector<int> s(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n; i++)	// 삽입 정렬
+	{
+		int insert_point = i;
+		int insert_value = a[i];
+
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (a[j] < a[i])
+			{
+				insert_point = j + 1;
+				break;
+			}
+			if (j == 0)
+			{
+				insert_point = 0;
+			}
+		}
+		for (int j = i; j > insert_point; j--)
+		{
+			a[j] = a[j - 1];
+		}a[insert_point] = insert_value;
+	}
+
+	s[0] = a[0];	// 합배열 만들기
+
+	for (int i = 1; i < n; i++)
+	{
+		s[i] = s[i - 1] + a[i];
+	}
+
+	int sum = 0;	// 합 배열 총합 구하기
+
+	for (int i = 0; i < n; i++)
+	{
+		sum = sum + s[i];
+	}
+	
+	cout << sum;
+}
