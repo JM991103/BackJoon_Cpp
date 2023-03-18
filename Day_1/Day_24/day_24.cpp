2aea3263f65f5c2256ae8cf7677642d848d7d00b
JM991103 <ljn1103@naver.com>
2023-03-18 15:35:51 +0900

    25일차 백준 풀기

--- Day_1/Day_24/day_24.cpp
+++ Day_1/Day_24/day_24.cpp
@@ -9,52 +9,79 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
-	vector<int> a(n, 0);
-	vector<int> s(n, 0);
+	//int n;
+	//cin >> n;
+	//vector<int> a(n, 0);
+	//vector<int> s(n, 0);
 
-	for (int i = 0; i < n; i++)
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cin >> a[i];
+	//}
+
+	//for (int i = 0; i < n; i++)	// 삽입 정렬
+	//{
+	//	int insert_point = i;
+	//	int insert_value = a[i];
+
+	//	for (int j = i - 1; j >= 0; j--)
+	//	{
+	//		if (a[j] < a[i])
+	//		{
+	//			insert_point = j + 1;
+	//			break;
+	//		}
+	//		if (j == 0)
+	//		{
+	//			insert_point = 0;
+	//		}
+	//	}
+	//	for (int j = i; j > insert_point; j--)
+	//	{
+	//		a[j] = a[j - 1];
+	//	}a[insert_point] = insert_value;
+	//}
+
+	//s[0] = a[0];	// 합배열 만들기
+
+	//for (int i = 1; i < n; i++)
+	//{
+	//	s[i] = s[i - 1] + a[i];
+	//}
+
+	//int sum = 0;	// 합 배열 총합 구하기
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	sum = sum + s[i];
+	//}
+	//
+	//cout << sum;
+
+	//int a, b;
+	//cin >> a;
+	//cin >> b;
+
+	//cout << a + b;
+
+	int a, b;
+
+	while (1)
 	{
-		cin >> a[i];
-	}
+		cin >> a >> b;
 
-	for (int i = 0; i < n; i++)	// 삽입 정렬
-	{
-		int insert_point = i;
-		int insert_value = a[i];
-
-		for (int j = i - 1; j >= 0; j--)
+		if (a == 0 && b == 0)
 		{
-			if (a[j] < a[i])
-			{
-				insert_point = j + 1;
-				break;
-			}
-			if (j == 0)
-			{
-				insert_point = 0;
-			}
+			break;
 		}
-		for (int j = i; j > insert_point; j--)
+
+		if (a > b)
 		{
-			a[j] = a[j - 1];
-		}a[insert_point] = insert_value;
+			cout << "Yes" << "\n";
+		}
+		else
+		{
+			cout << "No" << "\n";
+		}
 	}
-
-	s[0] = a[0];	// 합배열 만들기
-
-	for (int i = 1; i < n; i++)
-	{
-		s[i] = s[i - 1] + a[i];
-	}
-
-	int sum = 0;	// 합 배열 총합 구하기
-
-	for (int i = 0; i < n; i++)
-	{
-		sum = sum + s[i];
-	}
-	
-	cout << sum;
 }
