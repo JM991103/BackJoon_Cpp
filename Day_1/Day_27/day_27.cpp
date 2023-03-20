b8eb76f21a966fe075d5090ac78b33eadcd69e63
JM991103 <ljn1103@naver.com>
2023-03-20 19:48:11 +0900

    27일차 백준 풀기

---
+++ Day_1/Day_27/day_27.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+//#include <algorithm>
+using namespace std;
+
+void quickSort(vector<int>& A, int S, int E, int K);
+int partition(vector<int>& A, int S, int E);
+void swap(vector<int>& A, int i, int j);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n, 0);
+
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cin >> a[i];
+	//}
+
+	//sort(a.begin(), a.end());
+
+	//cout << a[k - 1];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	quickSort(a, 0, n - 1, k - 1);
+	cout << a[k - 1];
+}
+
+void quickSort(vector<int>& A, int S, int E, int K)
+{
+	int pivot = partition(A, S, E);
+	if (pivot == K)	// K번째 수가 pivot이면 더는 구할 필요 없음
+	{
+		return;
+	}
+	else if (K < pivot) {	// K가 pivot보다 작으면 왼쪽 그룹만 정렬 수행하기
+		quickSort(A, S, pivot - 1, K);
+	}
+	else
+	{
+		quickSort(A, pivot + 1, E, K);
+	}
+}
+
+int partition(vector<int>& A, int S, int E)
+{
+	// 데이터가 2개인 경우에는 바로 정렬
+	if (S + 1 == E)
+	{
+		if (A[S] > A[E]) 
+		{
+			swap(A, S, E);
+		}
+		return E;
+	}
+
+	int M = (S + E) / 2;
+	swap(A, S, M);	// 중앙값을 1번째 요소로 이동
+	int pivot = A[S];
+	int i = S + 1, j = E;
+
+	while (i <= j)
+	{
+		while (pivot < A[j] && j > 0)	// 피벗보다 작은 수가 나올 때까지 j--
+		{
+			j--;
+		}
+		while (pivot > A[i] && i < A.size() - 1)	// 피벗보다 큰 수가 나올 때까지 i++
+		{
+			i++;
+		}
+		if (i <= j)
+		{
+			swap(A, i++, j--);	// 찾은 i와 j 교환하기
+		}
+	}
+	// i == j 피벗의 값을 양쪽으로 분리한 가운데에 오도록 설정하기
+	A[S] = A[j];
+	A[j] = pivot;
+	return j;
+}
+
+void swap(vector<int>& A, int i, int j)
+{
+	int temp = A[i];
+	A[i] = A[j];
+	A[j] = temp;
+}
