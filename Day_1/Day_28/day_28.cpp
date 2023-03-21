352b7f934392ff96fcc662491b2269e3e74b2c4e
JM991103 <ljn1103@naver.com>
2023-03-21 20:13:23 +0900

    28일차 백준 풀기

---
+++ Day_1/Day_28/day_28.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < m)
+		{
+			cout << a[i] << " ";
+		}
+	}
+}
