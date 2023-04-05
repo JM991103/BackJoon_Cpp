7c599daf395592d56889f5faf4adff56716de341
JM991103 <ljn1103@naver.com>
2023-04-05 11:47:05 +0900

    43일차 백준 풀기

--- Day_1/Day_38/day_38.cpp
+++ Day_1/Day_38/day_38.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -42,7 +43,7 @@ int main()
     }
     cout << temp / 5;*/
 
-    int n, m;
+    /*int n, m;
 
     int temp = 0;
     int max = 0;
@@ -55,5 +56,25 @@ int main()
             max = temp;
         }
     }
-    cout << max;
+    cout << max;*/
+
+    int a, b, num;
+    int arr[] = { 1,2,3 };
+
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> a >> b;
+        swap(arr[a - 1], arr[b - 1]);        
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (arr[i] == 1)
+        {
+            cout << i + 1;
+        }
+    }
+
 }
