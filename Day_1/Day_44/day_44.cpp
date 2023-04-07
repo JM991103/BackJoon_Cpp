1e386edcca135db22f3a92c4e777f020a21e3694
JM991103 <ljn1103@naver.com>
2023-04-07 18:16:51 +0900

    45일차 백준 풀기

--- Day_1/Day_44/day_44.cpp
+++ Day_1/Day_44/day_44.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 using namespace std;
+
+int fun(int num);
 
 int main()
 {
@@ -8,26 +11,65 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int total[5];
-    int num;
-    int max = 0;
-    int id;
+    /*   int total[5];
+       int num;
+       int max = 0;
+       int id;
 
-    for (int i = 0; i < 5; i++)
+       for (int i = 0; i < 5; i++)
+       {
+           total[i] = 0;
+
+           for (int j = 0; j < 4; j++)
+           {
+               cin >> num;
+               total[i] += num;
+           }
+
+           if (max < total[i])
+           {
+               max = total[i];
+               id = i;
+           }
+       }
+       cout << id + 1 << " " << max << "\n";*/
+
+       /*long long n, m;
+       long long temp;
+
+       cin >> n >> m;
+       cin >> temp;
+
+       if (n + m - 2 * temp >= 0)
+       {
+           cout << n + m - 2 * temp << "\n";
+       }
+       else
+       {
+           cout << n + m << "\n";
+       }*/
+
+    /*int n, m;
+
+    while (cin >> n >> m)
     {
-        total[i] = 0;
+        cout << n + m << "\n";
+    }*/
 
-        for (int j = 0; j < 4; j++)
-        {
-            cin >> num;
-            total[i] += num;
-        }
+    int n;
+    cin >> n;    
 
-        if (max < total[i])
-        {
-            max = total[i];
-            id = i;
-        }
+    int result = fun(n);
+
+    cout << result << "\n";
+    
+}
+
+int fun(int num)
+{
+    if (num <= 1)
+    {
+        return 1;
     }
-    cout << id + 1 << " " << max << "\n";
+    return num * fun(num - 1);
 }
