cebefac7efbd6025eaf4278f2c646c458d184763
JM991103 <ljn1103@naver.com>
2023-04-08 17:30:02 +0900

    46일차 백준 풀기

---
+++ Day_1/Day_46/day_46.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int Fibo(int num);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m = 0;
+    cin >> n;
+    m = Fibo(n);
+    cout << m;
+}
+
+int Fibo(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    if (num == 1)
+    {
+        return 1;
+    }
+
+    return Fibo(num - 2) + Fibo(num - 1);
+}
