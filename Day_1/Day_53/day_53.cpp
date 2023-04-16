43bec5e042267061ed5988bde68f937524a3724a
JM991103 <ljn1103@naver.com>
2023-04-16 19:09:25 +0900

    54일차 백준 풀기

--- Day_1/Day_53/day_53.cpp
+++ Day_1/Day_53/day_53.cpp
@@ -8,26 +8,35 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string name;
-    int age, weight;
+    //string name;
+    //int age, weight;
 
-    while (true)
-    {
-        cin >> name >> age >> weight;
+    //while (true)
+    //{
+    //    cin >> name >> age >> weight;
 
-        if (name == "#" && age == 0 && weight == 0)
-        {
-            break;
-        }
+    //    if (name == "#" && age == 0 && weight == 0)
+    //    {
+    //        break;
+    //    }
 
-        if (age > 17 || weight >= 80)
-        {
-            cout << name << " Senior" << "\n";
-        }
-        else
-        {
-            cout << name << " Junior" << "\n";
-        }
-    }
+    //    if (age > 17 || weight >= 80)
+    //    {
+    //        cout << name << " Senior" << "\n";
+    //    }
+    //    else
+    //    {
+    //        cout << name << " Junior" << "\n";
+    //    }
+    //}
 
+    int burger1, burger2, burger3, coke, sprite;
+
+    cin >> burger1 >> burger2 >> burger3 >> coke >> sprite;
+    
+    int burgerMin = min(burger1, burger2);
+    burgerMin = min(burgerMin, burger3);
+    int drinkMin = min(coke, sprite);
+
+    cout << burgerMin + drinkMin - 50 << "\n";
 }
