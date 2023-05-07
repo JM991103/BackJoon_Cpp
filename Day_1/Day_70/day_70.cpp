a67e5053af2f05164862c4c5289f08cad9d5e24a
JM991103 <ljn1103@naver.com>
2023-05-07 21:23:39 +0900

    72일차 백준 풀기

--- Day_1/Day_70/day_70.cpp
+++ Day_1/Day_70/day_70.cpp
@@ -30,7 +30,7 @@ int main()
 	//}
 	//cout << score;
 
-	int ans[10000] = { 10000 }, n, k, cnt = 0, p = 0;
+	/*int ans[10000] = { 10000 }, n, k, cnt = 0, p = 0;
 
 	cin >> n >> k;
 
@@ -54,5 +54,29 @@ int main()
 	else
 	{
 		cout << ans[k - 1];
+	}*/
+
+	int t;
+	cin >> t;
+
+	while (t--) 
+	{
+		int n;
+
+		cin >> n;
+
+		string stringN = to_string(n);
+
+		string stringSquare = to_string(n * n);
+
+		if (stringSquare.find(stringN) == stringSquare.size() - stringN.size())
+		{
+			cout << "YES\n";
+		}
+		else 
+		{
+			cout << "NO\n";
+		}
 	}
+
 }
