4155b38ef8d5da8eca08cfb32d18b53a1544f0bd
JM991103 <ljn1103@naver.com>
2023-05-13 17:14:46 +0900

    78일차 백준 풀기

--- Day_1/Day_70/day_70.cpp
+++ Day_1/Day_70/day_70.cpp
@@ -136,29 +136,49 @@ int main()
 		cout << "2\n";
 	}*/
 
-	int a, b;
-	cin >> a >> b;
+	//int a, b;
+	//cin >> a >> b;
 
-	int n[1000];
-	int cnt = 0;
+	//int n[1000];
+	//int cnt = 0;
 
-	for (int i = 1; cnt < 1000; i++) 
+	//for (int i = 1; cnt < 1000; i++) 
+	//{
+	//	for (int j = 0; j < i; j++) 
+	//	{
+	//		n[cnt++] = i; //cnt는 for문과 상관없이 이 줄이 끝나면 1씩 증가
+	//		if (cnt == 1000) //n배열을 다 채우면 break;
+	//			
+	//		break;
+	//	}
+	//}
+
+	//int sum = 0;
+
+	//for (int i = a - 1; i <= b - 1; i++) //a번째는 n[a-1]값이고, b번째는 n[b-1]값 이므로
+	//{ 
+	//	sum += n[i];
+	//}
+	//cout << sum << endl;
+
+	int n;
+	cin >> n;
+
+	for (int t = 0; t < n; t++)
 	{
-		for (int j = 0; j < i; j++) 
+		int N;
+		cin >> N;
+
+		long long sum = 0;
+
+		for (int i = 0; i < N; i++)
 		{
-			n[cnt++] = i; //cnt는 for문과 상관없이 이 줄이 끝나면 1씩 증가
-			if (cnt == 1000) //n배열을 다 채우면 break;
-				
-			break;
+			long long candyCnt;
+			cin >> candyCnt;
+
+			sum = (sum + candyCnt) % N;
 		}
-	}
 
-	int sum = 0;
-
-	for (int i = a - 1; i <= b - 1; i++) //a번째는 n[a-1]값이고, b번째는 n[b-1]값 이므로
-	{ 
-		sum += n[i];
+		cout << (sum % N ? "NO\n" : "YES\n");
 	}
-	cout << sum << endl;
-	return 0;
 }
