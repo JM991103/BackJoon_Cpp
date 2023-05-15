6c4e57d9c0662687345cd98fa15ce0f8d992fb2f
JM991103 <ljn1103@naver.com>
2023-05-15 19:12:43 +0900

    80일차 백준 풀기

--- Day_1/Day_79/day_79.cpp
+++ Day_1/Day_79/day_79.cpp
@@ -5,21 +5,48 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
+	/*ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-    int k, d, a;
-    char trash;
+	int k, d, a;
+	char trash;
 
-    cin >> k >> trash >> d >> trash >> a;
+	cin >> k >> trash >> d >> trash >> a;
 
-    if (k + a < d || d == 0)
-    {
-        cout << "hasu";
-    }
-    else
-    {
-        cout << "gosu";
-    }
+	if (k + a < d || d == 0)
+	{
+		cout << "hasu";
+	}
+	else
+	{
+		cout << "gosu";
+	}*/
+
+	int n, tmp1, tmp2;
+
+	while (1)
+	{
+		cin >> n;
+
+		if (n == 0)
+		{
+			break;
+		}
+
+		while (n >= 10)
+		{
+			tmp1 = n, tmp2 = 0;
+
+			while (tmp1 > 0)
+			{
+				tmp2 += tmp1 % 10;
+				tmp1 /= 10;
+			}
+
+			n = tmp2;
+		}
+
+		cout << n << '\n';
+	}
 }
