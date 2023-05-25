c11b97b28c4d91839dc158ee3335104ee3daa42d
JM991103 <ljn1103@naver.com>
2023-05-25 17:38:24 +0900

    90일차 백준 풀기

--- Day_1/Day_79/day_79.cpp
+++ Day_1/Day_79/day_79.cpp
@@ -240,7 +240,7 @@ int main()
 	}
 	return 0;*/
 
-	string eight;
+	/*string eight;
 
 	cin >> eight;
 
@@ -282,6 +282,27 @@ int main()
 		else if (eight[i] == '6')
 			cout << "110";
 		else if (eight[i] == '7')
-			cout << "111";
+			cout << "111";*/
+			//}
+
+	int x, y, a, b;
+	cin >> x >> y;
+
+	a = (x + y) / 2;
+	b = (x - y) / 2;
+	
+	if ((x + y) % 2 != 0 || (x - y) % 2 != 0)
+		cout << -1 << '\n';
+	else
+	{
+		if (a >= 0 && b >= 0)
+		{
+			if (a >= b)
+				cout << a << ' ' << b << '\n';
+			else
+				cout << b << ' ' << a << '\n';
+		}
+		else
+			cout << -1 << '\n';
 	}
 }
