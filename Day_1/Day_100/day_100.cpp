578a984a880c2dde4a9271784fcce9345db3c241
JM991103 <ljn1103@naver.com>
2023-06-04 23:19:52 +0900

    100일차 백준 풀기

---
+++ Day_1/Day_100/day_100.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n, f;
+
+	cin >> n >> f;
+
+	n -= n % 100;	
+
+	for (int i = n; ; i++) 
+	{
+		if (i % f == 0)
+		{
+			if (i % 100 < 10)
+			{
+				cout << "0";
+			}
+
+			cout << i % 100;
+
+			break;
+		}
+
+	}
+}
